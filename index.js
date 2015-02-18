ee19ce31127d40d433c03051d1fd45a514cc977e
frederic <frederic@lust.nl>
2015-02-18 17:40:40 +0100

    sending email via mailgun config (with sensitive data) outsourced to config.js

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ winston.add(winston.transports.Console, {
 	},
 	colorize: true
 });
+var config = require('./config.js')
+
 
 var nopt = require('nopt');
 var opts = {
@@ -41,11 +43,11 @@ var argv = nopt(opts, shorthands, process.argv);
 // console.log(argv);
 
 
-var schedule = later.parse.recur().every(10).minute();
-// var next10 = later.schedule(schedule).next(10);
-var timer = later.setInterval(function() {
-	check_price();
-}, schedule);
+// var schedule = later.parse.recur().every(10).minute();
+// // var next10 = later.schedule(schedule).next(10);
+// var timer = later.setInterval(function() {
+// 	check_price();
+// }, schedule);
 
 
 function handle_error(err) {
@@ -73,22 +75,51 @@ process.on('SIGINT', function () {
 // db('posts').find({ title: 'lowdb is awesome' });
 
 
-var url = 'http://www.amazon.de/Dell-LED-Monitor-DisplayPort-Reaktionszeit-h%C3%B6henverstellbar/dp/B0091ME4A0/ref=sr_1_1?ie=UTF8&qid=1423474949&sr=8-1&keywords=dell+ultrasharp+u2713hm';
-function check_price() {
-	request(url, function(err, response, html) {
-		if (err) {
-			handle_error(err);
-			return;
-		}
 
-		var $ = cheerio.load(html);
-		var price = $('#priceblock_ourprice').text();
-		price = price.replace(/EUR/, '').replace(' ', '').replace(',', '.');
-		price = parseFloat(price);
-		winston.info(price);
+
+function send_email(subject, content) {
+	var nodemailer = require('nodemailer');
+	var mailgun = require('nodemailer-mailgun-transport');
+
+	var transporter = nodemailer.createTransport( mailgun(config.email.mailgun) );
+	
+	var mail = {
+		from: config.email.from,
+		to: config.email.to,
+		subject: subject,
+		html: content,
+		text: cheerio(content).text()
+	};
+
+	transporter.sendMail(mail, function(err, info) {
+		if (err) {
+			winston.error(err);
+		} else {
+			winston.info('Message sent: ' + info.response);
+		}
 	});
 }
-check_price();
+send_email('another test', '<a href="http://www.google.de">google</a>');
+
+
+
+
+// var url = 'http://www.amazon.de/Dell-LED-Monitor-DisplayPort-Reaktionszeit-h%C3%B6henverstellbar/dp/B0091ME4A0/ref=sr_1_1?ie=UTF8&qid=1423474949&sr=8-1&keywords=dell+ultrasharp+u2713hm';
+// function check_price() {
+// 	request(url, function(err, response, html) {
+// 		if (err) {
+// 			handle_error(err);
+// 			return;
+// 		}
+
+// 		var $ = cheerio.load(html);
+// 		var price = $('#priceblock_ourprice').text();
+// 		price = price.replace(/EUR/, '').replace(' ', '').replace(',', '.');
+// 		price = parseFloat(price);
+// 		winston.info(price);
+// 	});
+// }
+// check_price();
 
 
 
