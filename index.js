c67e194589ea1be51bd17b07635929d98e063124
frederic <frederic@lust.nl>
2015-02-18 18:28:32 +0100

    basic task creation function

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ var fs = require('fs');
 var path = require('path');
 var chalk = require('chalk');
 var later = require('later');
-var request = require('request');
-var FeedParser = require('feedparser');
 var split = require('split');
 var through2 = require('through2');
 var lowdb = require('lowdb');
-var cheerio = require('cheerio');
 var moment = require('moment');
 var winston = require('winston');
 winston.remove(winston.transports.Console);
@@ -44,14 +41,7 @@ var shorthands = {
 	// "f" : ["--flag"]
 };
 var argv = nopt(opts, shorthands, process.argv);
-// console.log(argv);
 
-
-// var schedule = later.parse.recur().every(10).minute();
-// // var next10 = later.schedule(schedule).next(10);
-// var timer = later.setInterval(function() {
-// 	check_price();
-// }, schedule);
 
 
 
@@ -78,12 +68,6 @@ process.on('SIGINT', function () {
 
 
 
-// var tasks = [
-// 	{
-// 		type: 
-// 	}
-// ]
-
 
 
 var url = 'http://www.amazon.de/Dell-LED-Monitor-DisplayPort-Reaktionszeit-h%C3%B6henverstellbar/dp/B0091ME4A0/ref=sr_1_1?ie=UTF8&qid=1423474949&sr=8-1&keywords=dell+ultrasharp+u2713hm';
@@ -91,21 +75,18 @@ var check_price = amazon.create_pricecheck({
 	url: url,
 	threshold: 400
 });
-check_price();
 
 
+function create_task(func, interval) {
+	// var schedule = later.parse.recur()
+	// 	.every(10).minute();
+	var schedule = later.parse.text(interval);
+	var timer = later.setInterval(func, schedule);
+}
 
-// var seen = {};
-// feedparser.on('readable', function() {
-// 	var stream = this;
-// 	var article;
-// 	while (article = stream.read()) {
-// 		var time = article.date.getTime();
-// 		if (!(article.guid in seen) || (seen[article.guid] !== 0 && seen[article.guid] != time)) {
-// 			seen[article.guid] = article.date ? time : 0;
-// 		}
-// 	}
-// });
+create_task(check_price, 'every 10 mins');
+
+
 
 
 
