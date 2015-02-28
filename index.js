36746054f1a28f27eb621224f7b2fb9feb5d4c97
frederic <frederic@lust.nl>
2015-02-28 19:23:35 +0100

    load tasks from db

--- index.js
+++ index.js
@@ -18,19 +18,18 @@ var db = require( path.join(global.paths.root, 'db.js') );
 var config = require( path.join(global.paths.root, 'config.js') );
 var server = require( path.join(global.paths.lib, 'server.js') );
 var helper = require( path.join(global.paths.lib, 'helper.js') );
-var task = require( path.join(global.paths.lib, 'task.js') );
+var tasklib = require( path.join(global.paths.lib, 'tasklib.js') );
 
 /*
 TODO:
-- load tasks from db
 - how to know when a task has ended?
 	- we don't need to know: every step should request a db save of the entire task, but we are simply going to throttle it.
-
 - clean up: task stuff into task.js
 - core functionality should be in /lib and not indivisual blocks
-- blocks should be able to do logging themselves config: { log: true }
+- blocks should be able to do logging themselves config: { log: false }
 WISH LIST:
 - different colors for different tasks
+	- white, grey, black, blue, cyan, green, magenta, red, yellow
 - project logo
 */
 
@@ -47,7 +46,7 @@ function list_tasks() {
 	console.log('———————————————');
 	console.log('TASKS');
 	// db('tasks').forEach(function(t) {
-	_.values(tasks).forEach(function(task) {
+	tasks.forEach(function(task) {
 		console.log( sf('- {0} ({1})', chalk.bgBlue(task.name), task.interval) );
 	});
 	console.log('———————————————');
@@ -102,211 +101,6 @@ process.on('SIGINT', function() {
 });
 
 
-
-var tasks = global.tasks = {
-	'dell-monitor': {
-		name: 'dell monitor',
-		interval: 'every 15 mins',
-
-		steps: [
-			{
-				id: 'price',
-				block: 'amazon-price',
-				options: {
-					url: 'http://www.amazon.de/Dell-LED-Monitor-DisplayPort-Reaktionszeit-h%C3%B6henverstellbar/dp/B0091ME4A0/ref=sr_1_1?ie=UTF8&qid=1423474949&sr=8-1&keywords=dell+ultrasharp+u2713hm',
-					currency: 'EUR'
-				},
-				flow: {
-					'if': [ // every time the price changes
-						'console',
-						'desktop-notification',
-						'threshold'
-					]
-				}
-			},
-			{
-				id: 'console',
-				block: 'log-console',
-				options: {
-					title: 'price changed',
-					message: '<%=format.money(input)%>'
-				},
-				flow: {}
-			},
-			{
-				id: 'threshold',
-				block: 'threshold',
-				options: {
-					value: 400,
-					comparison: '<='
-				},
-				flow: {
-					'if': [ // only send notifications, once price dropped below threshold
-						'email',
-						'pushover'
-					]
-				}
-			},
-			{
-				id: 'email',
-				block: 'email-notification',
-				options: {},
-				flow: {}
-			},
-			{
-				id: 'desktop-notification',
-				block: 'desktop-notification',
-				options: {
-					message: '<%=format.money(input)%>'
-				},
-				flow: {}
-			},
-			{
-				id: 'pushover',
-				block: 'pushover',
-				options: {
-					message: 'dell monitor price: <%=format.money(input)%>'
-				},
-				flow: {}
-			}
-		],
-
-		data: {} // TODO: keep track of how many times a task has been run
-	},
-
-	'btc-rate': {
-		name: 'bitcoin rate',
-		interval: 'every 10 mins',
-
-		steps: [
-			{
-				id: 'price',
-				block: 'bitcoin-rate',
-				options: {
-					'market': 'bitcoin_de'
-				},
-				flow: {
-					'anyway': [
-						'console',
-						'threshold'
-					]
-				}
-			},
-			{
-				id: 'console',
-				block: 'log-console',
-				options: {
-					title: 'atm',
-					message: '<%=format.money(input)%>'
-				},
-				flow: {}
-			},
-			{
-				id: 'threshold',
-				block: 'threshold',
-				options: {
-					'value': 250,
-					'comparison': '>='
-				},
-				flow: {
-					'if': [
-						'desktop-notification',
-						'email',
-						'pushover'
-					]
-				}
-			},
-			{
-				id: 'email',
-				block: 'email-notification',
-				options: {},
-				flow: {}
-			},
-			{
-				id: 'desktop-notification',
-				block: 'desktop-notification',
-				options: {
-					message: '<%=format.money(input)%>'
-				},
-				flow: {}
-			},
-			{
-				id: 'pushover',
-				block: 'pushover',
-				options: {
-					message: 'bitcoin rate: <%=format.money(input)%>'
-				},
-				flow: {}
-			}
-		]
-	},
-
-	'subbacultcha-mps3s': {
-		name: 'new subbacultcha music',
-		interval: 'every 1 hours',
-
-		steps: [
-			{
-				id: 'website-changed',
-				block: 'website-changed',
-				options: {
-					url: 'http://subbacultcha.nl/mp3-of-the-day',
-					selector: '#maincontent'
-				},
-				flow: {
-					'if': ['console']
-				}
-			},
-			{
-				id: 'console',
-				block: 'log-console',
-				options: {
-					title: 'new',
-					message: 'music'
-				},
-				flow: {}
-			}
-		]
-	},
-
-	'adventuretime-episodes': {
-		name: 'adventure time',
-		interval: 'every 2 hours',
-
-		steps: [
-			{
-				id: 'feedzzz',
-				block: 'feed',
-				options: {
-					'url': 'http://www.watchcartoononline.com/anime/adventure-time/feed'
-				},
-				flow: {
-					'if': ['console', 'email']
-				}
-			},
-			{
-				id: 'console',
-				block: 'log-console',
-				options: {
-					title: 'new episodes',
-					message: '<%var episodes = input.map(function(ep) { return ep.title; })%>\n<%=format.list(episodes)%>'
-				},
-				flow: {}
-			},
-			{
-				id: 'email',
-				block: 'email-notification',
-				options: {
-					title: 'new adventure time episode(s)',
-					message: '<%_.each(input, function(ep) {%>'+'<a href="'+'<%=ep.link%>'+'">'+'<%=ep.title%>'+'</a><br>'+'<%})%>'
-				},
-				flow: {}
-			},
-		]
-	}
-}
-
-
 function run_task(task) {
 	var tos = task.steps.reduce(function(result, step) {
 		result = result.concat(step.flow['if']);
@@ -333,9 +127,13 @@ function validate_step_flow(flow) {
 }
 
 
+var tasks = [];
 function load_tasks() {
-	_.values(tasks).forEach(function(task) {
-		// winston.info('loading task: ' + task.name);
+	// load tasks from db
+	db.object.tasks.forEach(function(_task) {
+		var task = _.extend({}, _task);
+
+		winston.info('loading task: ' + task.name);
 
 		task.steps.forEach(function(step) {
 			step.flow = validate_step_flow(step.flow);
@@ -353,11 +151,14 @@ function load_tasks() {
 			_schedule: schedule,
 			_timer: later.setInterval(run, schedule)
 		});
+
+		tasks.push(task);
+		_task = tasklib.make_savable(task);
 	});
 }
 
 load_tasks();
-_.values(tasks).forEach(function(task) {
+// global.tasks = tasks;
+tasks.forEach(function(task) {
 	task._run();
-	// console.log( later.schedule(task._schedule).next(2) );
 });
