946f6eae00b5f49e89eb3111a875a2ece053d52e
frederic <frederic@lust.nl>
2015-03-03 12:12:00 +0100

    automatically run tests + show notification if tests fail

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var notifier = require('node-notifier');
+
+
+gulp.task('mocha', function() {
+	return gulp.src('test/test.js', { read: false })
+		.pipe(mocha())
+		.on('error', function(err) {
+			notifier.notify({
+				title: err.plugin,
+				message: err.message
+			});
+		});
+});
+
+
+gulp.task('default', function() {
+	gulp.watch([
+			'./*.js',
+			'./lib/*.js',
+			'./blocks/*.js',
+			'./test/*.js'
+		],
+		['mocha']
+	);
+});
