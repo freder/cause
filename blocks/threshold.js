00ef1c3fca95e44880ddc26d3d0a4e65fb926385
frederic <frederic@lust.nl>
2015-03-04 09:50:35 +0100

    threshold: trigger only once

--- blocks/threshold.js
+++ blocks/threshold.js
@@ -2,6 +2,7 @@ var path = require('path');
 var _ = require('lodash');
 var versus = require('versus');
 
+var db = require( path.join(global.paths.root, 'db.js') );
 var helper = require( path.join(global.paths.lib, 'helper.js') );
 var tasklib = require( path.join(global.paths.lib, 'tasklib.js') );
 
@@ -12,17 +13,31 @@ function create(task, step) {
 		// comparison: '=='
 	};
 	step.options = tasklib.normalize_step_options(step, defaults);
-	step.data = tasklib.normalize_step_data(step);
+	var data_defaults = {
+		triggered: false
+	};
+	step.data = tasklib.normalize_step_data(step, data_defaults);
 
 	return function(input, prev_step) {
+		var output = input;
+
+		// has the threshold been crossed?
 		var check = versus(input, step.options.comparison, step.options.value);
 		var flow_decision = tasklib.flow_decision(check);
 
-		// pass through
-		var output = input;
+		// trigger only once, when the threshold is reached,
+		// otherwise it would keep on triggering.
+		// TODO: maybe make desired behavior configurable
+		if (check && step.data.triggered) {
+			flow_decision['if'] = false;
+			flow_decision['else'] = false;
+		}
 
-		// invoke children
 		tasklib.invoke_children(step, task, output, flow_decision);
+
+		// mark as triggered, or not
+		step.data.triggered = check;
+		db.save(); // TODO: save automatically from where this function is called?
 	};
 }
 
