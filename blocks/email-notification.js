d0f3ded9e3e93f2db5c7ae050aedbf7f813506f3
frederic <frederic@lust.nl>
2015-03-22 14:05:28 +0100

    possibility to override email defaults

--- blocks/email-notification.js
+++ blocks/email-notification.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var _ = require('lodash');
 
+var config = require( path.join(global.paths.root, 'config.js') );
 var helper = require( path.join(global.paths.lib, 'helper.js') );
 var tasklib = require( path.join(global.paths.lib, 'tasklib.js') );
 var email = require( path.join(global.paths.lib, 'email.js') );
@@ -12,7 +13,13 @@ function fn(task, step, input, prev_step) {
 	var title = _.template(step.options.title)(message_vars);
 	var message = _.template(step.options.message)(message_vars);
 
+	// override email defaults
+	var to = (step.options.to) ? step.options.to : config.email.to;
+	var from = (step.options.from) ? step.options.from : config.email.from;
+
 	email.send({
+		from: from,
+		to: to,
 		subject: title,
 		html: message
 	});
@@ -27,6 +34,8 @@ module.exports = {
 	fn: fn,
 	defaults: {
 		options: {
+			from: undefined,
+			to: undefined,
 			title: "'cause: <%=task.name%>",
 			message: "<%=prev_step.block%>: <%=input%>"
 		},
