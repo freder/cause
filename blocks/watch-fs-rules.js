c631a3113b6031fc8cee088b5bcaa4e3dfdacbbf
freder <freder.code@gmail.com>
2015-06-18 17:09:16 +0200

    block to react on filesystem events

---
+++ blocks/watch-fs-rules.js
@@ -0,0 +1,13 @@
+module.exports = [
+	{
+		watch: ['/Users/fredericbrodbeck/Desktop/_screenshots/*.png'],
+		// ignore: /[\/\\]\./,
+		on: {
+			add: function(path) {
+				console.log(path, 'added');
+			},
+			// change: ...
+			// unlink: ...
+		}
+	}
+];
