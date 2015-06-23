2b5c8175ce6ea783dae1c9d21af474148da0ee26
freder <freder.code@gmail.com>
2015-06-23 19:42:56 +0200

    only show selected task

--- web/js/App.js
+++ web/js/App.js
@@ -13,20 +13,50 @@ var App = React.createClass({
 		return (
 			<div>
 				<div id="menu">
-					<h1>'cause</h1>
+					<strong>'cause</strong><br />
+					<br />
 					<div id="task-list">
-
+						{_.keys(this.props.tasks).map(this.renderMenuItem)}
 					</div>
 				</div>
 				<div id="tasks">
-					{_.values(this.props.tasks).map(this.renderTask)}
+					{/*_.values(this.props.tasks).map(this.renderTask)*/}
+					{this.renderSelectedTask()}
 				</div>
 			</div>
 		);
 	},
 
-	renderTask: function(task, index) {
-		return <Task task={task} key={index} />;
+	selectTask: function(name, event) {
+		event.preventDefault();
+		this.setState({ selectedTask: name });
+	},
+
+	renderMenuItem: function(name, index) {
+		var that = this;
+		return (
+			<div className='menu-item' key={index}>
+				<a href="#" onClick={_.partial(that.selectTask, name)}>{name}</a>
+			</div>
+		);
+	},
+
+	renderSelectedTask: function() {
+		var state = this.state;
+		if (!state.selectedTask) return null;
+		var props = this.props;
+		var task = props.tasks[state.selectedTask];
+		return this.renderTask(task, task.slug);
+	},
+
+	renderTask: function(task, key) {
+		return <Task task={task} key={key} />;
+	},
+
+	getInitialState: function() {
+		return {
+			selectedTask: false
+		};
 	}
 });
 
