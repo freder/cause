c6bb96afa2dee6b457ac6e0e8c0bdf89caf8991d
freder <freder.code@gmail.com>
2016-02-12 18:11:40 +0100

    const

--- blocks/threshold.js
+++ blocks/threshold.js
@@ -1,19 +1,22 @@
 'use strict';
 
-var taskUtils = require('cause-utils/task');
-var versus = require('versus');
+const versus = require('versus');
 
 
 function fn(input, step, context, done) {
-	var output = input;
+	const output = input;
 
 	// has the threshold been crossed?
-	var checkResult = versus(input, step.options.comparison, step.options.value);
-	var decision = taskUtils.flowDecision(checkResult);
+	const checkResult = versus(
+		input,
+		step.options.comparison,
+		step.options.value
+	);
+	let decision = context.prepareDecision(checkResult);
 
 	// trigger only once, when the threshold is reached,
 	// otherwise it would keep on triggering.
-	// TODO: maybe make desired behavior configurable
+	// TODO: make desired behavior configurable
 	if (checkResult && step.data.triggered) {
 		decision['if'] = false;
 		decision['else'] = false;
